2a2706cb4d7c4998365afad9e46e79451608d007
Mingming Li <21374618@qq.com>
2013-02-01 20:02:50 +0800

    a

---
+++ SunMoon.Blog.Mvc3Razor/Controllers/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SunMoon.Blog.DataAccess.EntityFramework;
+
+namespace SunMoon.Blog.Mvc3Razor.Controllers
+{
+    public class MessageController : BaseController
+    {
+        //
+        // GET: /Message/
+
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult GetMessages() {
+            using (blogEntities ct = new blogEntities()) {
+                var lists = ct.Messages.Where(m => m.Owner.Equals(Passport.AccountID)).ToList();
+
+                Models.Messages model = new Models.Messages();
+
+                model.Store = lists;
+
+                return View(model);
+            }
+        }
+    }
+}
---
+++ SunMoon.Blog.Mvc3Razor/Models/PageInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SunMoon.Blog.Mvc3Razor.Models {
+
+    public class PageInfo {
+
+        public PageInfo()
+            : this(10, 0) {
+        }
+
+        public PageInfo(int pageSize, int recordCount) {
+            this.PageSize = PageSize;
+            this.RecordCount = recordCount;
+            this.CurrentPageIndex = 1;
+        }
+
+        /// <summary>
+        /// 当前页
+        /// </summary>
+        public int CurrentPageIndex { get; set; }
+
+        /// <summary>
+        /// 页大小
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 总记录数
+        /// </summary>
+        public int RecordCount { get; set; }
+
+        /// <summary>
+        /// 页总数
+        /// </summary>
+        public int PageCount {
+            get {
+                if (PageSize > 0) {
+                    if (RecordCount % PageSize == 0)
+                        return RecordCount / PageSize;
+                    else
+                        return RecordCount / PageSize + 1;
+                }
+
+                return 0;
+            }
+        }
+    }
+}
